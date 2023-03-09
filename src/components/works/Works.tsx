bf0fcd6aa32891fb0ebe3baf1d43396581239c99
Arthur Ishmiev <artur.ishmiev@gmail.com>
2023-03-09 15:35:46 +0300

    refactor works state

--- src/components/works/Works.tsx
+++ src/components/works/Works.tsx
@@ -3,34 +3,24 @@ import container from "../../common/styles/Container.module.css";
 import styles from "./Works.module.css"
 import {Work} from "./work/Work";
 import {SectionTitle} from "../SectionTitle/SectionTitle";
-import work1Image from "../../common/img/work-img1.jpg"
+import {WorksState} from "./works-state";
 
 
 type SkillsPropsType = {}
 
 export const Works: FC<SkillsPropsType> = () => {
-
+    const WorksData = WorksState
     return (
         <section className={styles.worksBlock}>
             <div className={`${container.container} ${styles.worksContainer}`}>
                 <SectionTitle title={"My works"}
                               subTitle={'I design and develop services for customers of all sizes, specializing in creating stylish, modern websites, web services and online stores. My passion is to design digital user experiences through the bold interface.'}/>
                 <div className={styles.works}>
-                    <Work title={"TodoList"}
-                          description={"blablabl blablablablabl ablablablabla ablablablabla ablablablabla ablablablabla ablablablabla"}
-                          icon={work1Image}/>
-                    <Work title={"Social Network"}
-                          description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"}
-                          icon={work1Image}/>
-                    <Work title={"Social Network"}
-                          description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"}
-                          icon={work1Image}/>
-                    <Work title={"Social Network"}
-                          description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"}
-                          icon={work1Image}/>
-                    <Work title={"Social Network"}
-                          description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"}
-                          icon={work1Image}/>
+                    {
+                        WorksData.map((work, id)=> <Work title={work.title}
+                              description={work.subtitle}
+                              icon={work.icon}/>)
+                    }
                 </div>
             </div>
         </section>
---
+++ src/components/works/works-state.ts
@@ -0,0 +1,35 @@
+import work1Image from "../../common/img/work-img1.jpg"
+
+type WorkStateType = {
+    title: string
+    subtitle: string
+    icon: string
+}
+
+export const WorksState: WorkStateType[] = [
+    {
+        title: "TodoList",
+        subtitle: "blablabl blablablablabl ablablablabla ablablablabla ablablablabla ablablablabla ablablablabla",
+        icon: work1Image
+    },
+    {
+        title: "Social Network",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        icon: work1Image
+    },
+    {
+        title: "Social Network",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        icon: work1Image
+    },
+    {
+        title: "Social Network",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        icon: work1Image
+    },
+    {
+        title: "Social Network",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        icon: work1Image
+    },
+]
