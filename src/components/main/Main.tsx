5f0ce50923f41dd46a0ee4984446b2ce5a3edee2
Arthur Ishmiev <artur.ishmiev@gmail.com>
2023-03-15 16:40:47 +0300

    feat: styles for home section

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -2,7 +2,6 @@ import {FC} from "react";
 import styles from "./Main.module.css"
 import container from "../../common/styles/Container.module.css"
 
-
 type MainType = {}
 
 export const Main: FC<MainType> = () => {
@@ -10,14 +9,22 @@ export const Main: FC<MainType> = () => {
     return (
         <section id={"home"} className={styles.mainBlock}>
             <div className={`${container.container}`}>
-                <div className={styles.text}>
-                    <span>Hi there</span>
-                    <h1>"Arthur Ishmiev"</h1>
-                    <p>I'm frontend developer</p>
+                <div className={styles.row}>
+                    <div className={styles.contentWrapper}>
+                        <div className={styles.content}>
+                            <h4>Hello, my name is</h4>
+                            <h5>Arthur Ishmiev</h5>
+                            <p>I'm the frontend based in Canada. I make the visual to be more interactive.</p>
+                            <div className={styles.cvLinkWrapper}>
+                                <a href="#" className={styles.cvLink}>Download CV</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className={styles.photo}></div>
             </div>
-
+            <a href="#skills" className={styles.mouseIcon}>
+                <span className={styles.wheel}></span>
+            </a>
         </section>
     )
 }
